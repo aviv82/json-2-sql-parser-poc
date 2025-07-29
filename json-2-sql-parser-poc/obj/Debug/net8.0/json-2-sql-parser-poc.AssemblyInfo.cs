830f8594c6a49c55f2123f2741a14f830a5f5ca3
Aviv <aviv.dror@gmail.com>
2025-07-29 21:08:02 +0200

    initial

--- json-2-sql-parser-poc/obj/Debug/net8.0/json-2-sql-parser-poc.AssemblyInfo.cs
+++ json-2-sql-parser-poc/obj/Debug/net8.0/json-2-sql-parser-poc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("json-2-sql-parser-poc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7422c84a285c2932aedf1a9abde95a372add7446")]
 [assembly: System.Reflection.AssemblyProductAttribute("json-2-sql-parser-poc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("json-2-sql-parser-poc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
